898c3e0f978f591ff97ebf59aa7d638d7ba67b34
GlebsVituskins <glebva2202@gmail.com>
2025-02-17 16:16:06 +0200

    gray colors

--- app/src/main/java/com/gvituskins/utilityhelper/presentation/theme/Color.kt
+++ app/src/main/java/com/gvituskins/utilityhelper/presentation/theme/Color.kt
@@ -2,76 +2,92 @@ package com.gvituskins.utilityhelper.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val primaryLight = Color(0xFF415F91)
-val onPrimaryLight = Color(0xFFFFFFFF)
-val primaryContainerLight = Color(0xFFD6E3FF)
-val onPrimaryContainerLight = Color(0xFF001B3E)
-val secondaryLight = Color(0xFF565F71)
-val onSecondaryLight = Color(0xFFFFFFFF)
-val secondaryContainerLight = Color(0xFFDAE2F9)
-val onSecondaryContainerLight = Color(0xFF131C2B)
-val tertiaryLight = Color(0xFF535A92)
-val onTertiaryLight = Color(0xFFFFFFFF)
-val tertiaryContainerLight = Color(0xFFDFE0FF)
-val onTertiaryContainerLight = Color(0xFF0D154B)
-val errorLight = Color(0xFFBA1A1A)
+// Светлая тема
+val primaryLight = Color(0xFF000000) // Чёрный
+val onPrimaryLight = Color(0xFFFFFFFF) // Белый
+val primaryContainerLight = Color(0xFF333333) // Тёмно-серый
+val onPrimaryContainerLight = Color(0xFFE0E0E0) // Светло-серый
+
+val secondaryLight = Color(0xFF1A1A1A) // Очень тёмный серый
+val onSecondaryLight = Color(0xFFF5F5F5) // Почти белый
+val secondaryContainerLight = Color(0xFF4D4D4D) // Средне-серый
+val onSecondaryContainerLight = Color(0xFFD9D9D9) // Светло-серый
+
+val tertiaryLight = Color(0xFF262626) // Глубокий тёмно-серый
+val onTertiaryLight = Color(0xFFFAFAFA) // Почти белый
+val tertiaryContainerLight = Color(0xFF595959) // Серый средней яркости
+val onTertiaryContainerLight = Color(0xFFCCCCCC) // Светло-серый
+
+val errorLight = Color(0xFFB00020) // Красный (ошибки)
 val onErrorLight = Color(0xFFFFFFFF)
-val errorContainerLight = Color(0xFFFFDAD6)
+val errorContainerLight = Color(0xFFFFDAD4) // Светлый красный фон
 val onErrorContainerLight = Color(0xFF410002)
-val backgroundLight = Color(0xFFF9F9FF)
-val onBackgroundLight = Color(0xFF191C20)
-val surfaceLight = Color(0xFFF9F9FF)
-val onSurfaceLight = Color(0xFF191C20)
-val surfaceVariantLight = Color(0xFFE0E2EC)
-val onSurfaceVariantLight = Color(0xFF44474E)
-val outlineLight = Color(0xFF74777F)
-val outlineVariantLight = Color(0xFFC4C6D0)
-val scrimLight = Color(0xFF000000)
-val inverseSurfaceLight = Color(0xFF2E3036)
-val inverseOnSurfaceLight = Color(0xFFF0F0F7)
-val inversePrimaryLight = Color(0xFFAAC7FF)
-val surfaceDimLight = Color(0xFFD9D9E0)
-val surfaceBrightLight = Color(0xFFF9F9FF)
-val surfaceContainerLowestLight = Color(0xFFFFFFFF)
-val surfaceContainerLowLight = Color(0xFFF3F3FA)
-val surfaceContainerLight = Color(0xFFEDEDF4)
-val surfaceContainerHighLight = Color(0xFFE7E8EE)
-val surfaceContainerHighestLight = Color(0xFFE2E2E9)
 
-val primaryDark = Color(0xFFAAC7FF)
-val onPrimaryDark = Color(0xFF0A305F)
-val primaryContainerDark = Color(0xFF284777)
-val onPrimaryContainerDark = Color(0xFFD6E3FF)
-val secondaryDark = Color(0xFFBEC6DC)
-val onSecondaryDark = Color(0xFF283141)
-val secondaryContainerDark = Color(0xFF3E4759)
-val onSecondaryContainerDark = Color(0xFFDAE2F9)
-val tertiaryDark = Color(0xFFBCC3FF)
-val onTertiaryDark = Color(0xFF242C61)
-val tertiaryContainerDark = Color(0xFF3B4279)
-val onTertiaryContainerDark = Color(0xFFDFE0FF)
-val errorDark = Color(0xFFFFB4AB)
-val onErrorDark = Color(0xFF690005)
-val errorContainerDark = Color(0xFF93000A)
-val onErrorContainerDark = Color(0xFFFFDAD6)
-val backgroundDark = Color(0xFF111318)
-val onBackgroundDark = Color(0xFFE2E2E9)
-val surfaceDark = Color(0xFF111318)
-val onSurfaceDark = Color(0xFFE2E2E9)
-val surfaceVariantDark = Color(0xFF44474E)
-val onSurfaceVariantDark = Color(0xFFC4C6D0)
-val outlineDark = Color(0xFF8E9099)
-val outlineVariantDark = Color(0xFF44474E)
-val scrimDark = Color(0xFF000000)
-val inverseSurfaceDark = Color(0xFFE2E2E9)
-val inverseOnSurfaceDark = Color(0xFF2E3036)
-val inversePrimaryDark = Color(0xFF415F91)
-val surfaceDimDark = Color(0xFF111318)
-val surfaceBrightDark = Color(0xFF37393E)
-val surfaceContainerLowestDark = Color(0xFF0C0E13)
-val surfaceContainerLowDark = Color(0xFF191C20)
-val surfaceContainerDark = Color(0xFF1D2024)
-val surfaceContainerHighDark = Color(0xFF282A2F)
-val surfaceContainerHighestDark = Color(0xFF33353A)
+val backgroundLight = Color(0xFFFFFFFF) // Белый
+val onBackgroundLight = Color(0xFF000000) // Чёрный
 
-val appColor = Color(0xFF1C2533)
+val surfaceLight = Color(0xFFFAFAFA) // Почти белый
+val onSurfaceLight = Color(0xFF121212) // Почти чёрный
+val surfaceVariantLight = Color(0xFFD6D6D6) // Светло-серый
+val onSurfaceVariantLight = Color(0xFF3C3C3C) // Тёмно-серый
+
+val outlineLight = Color(0xFF9E9E9E) // Серый для границ
+val outlineVariantLight = Color(0xFFBDBDBD) // Чуть светлее
+val scrimLight = Color(0x99000000) // Полупрозрачный чёрный
+
+val inverseSurfaceLight = Color(0xFF121212) // Почти чёрный
+val inverseOnSurfaceLight = Color(0xFFFAFAFA) // Почти белый
+val inversePrimaryLight = Color(0xFFFFFFFF) // Инверсия Primary
+
+val surfaceDimLight = Color(0xFFE0E0E0) // Тусклый серый
+val surfaceBrightLight = Color(0xFFF5F5F5) // Светлый серый
+val surfaceContainerLowestLight = Color(0xFFFFFFFF) // Белый
+val surfaceContainerLowLight = Color(0xFFF0F0F0) // Очень светлый серый
+val surfaceContainerLight = Color(0xFFEAEAEA) // Светлый серый
+val surfaceContainerHighLight = Color(0xFFDCDCDC) // Серый
+val surfaceContainerHighestLight = Color(0xFFC0C0C0) // Самый насыщенный серый
+
+// Тёмная тема
+val primaryDark = Color(0xFFFFFFFF) // Белый
+val onPrimaryDark = Color(0xFF000000) // Чёрный
+val primaryContainerDark = Color(0xFF777777) // Серый
+val onPrimaryContainerDark = Color(0xFF222222) // Тёмно-серый
+
+val secondaryDark = Color(0xFFBDBDBD) // Светлый серый
+val onSecondaryDark = Color(0xFF000000) // Чёрный
+val secondaryContainerDark = Color(0xFF555555) // Средне-тёмный серый
+val onSecondaryContainerDark = Color(0xFFF5F5F5) // Почти белый
+
+val tertiaryDark = Color(0xFFA0A0A0) // Серый средней яркости
+val onTertiaryDark = Color(0xFF121212) // Почти чёрный
+val tertiaryContainerDark = Color(0xFF666666) // Средний серый
+val onTertiaryContainerDark = Color(0xFFEAEAEA) // Светло-серый
+
+val errorDark = Color(0xFFFFB4AB) // Светло-красный
+val onErrorDark = Color(0xFF690005) // Тёмно-красный
+val errorContainerDark = Color(0xFF93000A) // Интенсивный красный
+val onErrorContainerDark = Color(0xFFFFDAD4)
+
+val backgroundDark = Color(0xFF000000) // Чёрный
+val onBackgroundDark = Color(0xFFFFFFFF) // Белый
+
+val surfaceDark = Color(0xFF121212) // Почти чёрный
+val onSurfaceDark = Color(0xFFFAFAFA) // Почти белый
+val surfaceVariantDark = Color(0xFF444444) // Средне-тёмный серый
+val onSurfaceVariantDark = Color(0xFFD6D6D6) // Светло-серый
+
+val outlineDark = Color(0xFFBDBDBD) // Серый для границ
+val outlineVariantDark = Color(0xFF9E9E9E) // Чуть темнее
+val scrimDark = Color(0x99000000) // Полупрозрачный чёрный
+
+val inverseSurfaceDark = Color(0xFFFAFAFA) // Почти белый
+val inverseOnSurfaceDark = Color(0xFF121212) // Почти чёрный
+val inversePrimaryDark = Color(0xFF000000) // Инверсия Primary
+
+val surfaceDimDark = Color(0xFF222222) // Очень тёмный серый
+val surfaceBrightDark = Color(0xFF444444) // Серый
+val surfaceContainerLowestDark = Color(0xFF121212) // Чёрный
+val surfaceContainerLowDark = Color(0xFF1C1C1C) // Очень тёмный серый
+val surfaceContainerDark = Color(0xFF292929) // Средний тёмный серый
+val surfaceContainerHighDark = Color(0xFF3B3B3B) // Тёмный серый
+val surfaceContainerHighestDark = Color(0xFF4E4E4E) // Светлый серый
